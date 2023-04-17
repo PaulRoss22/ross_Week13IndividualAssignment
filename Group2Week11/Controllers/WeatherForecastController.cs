2dd03e7175178dc0b3944a8f9696e296d63a7489
PaulRoss22 <95063507+PaulRoss22@users.noreply.github.com>
2023-04-16 21:38:16 -0500

    commit

--- Group2Week11/Controllers/WeatherForecastController.cs
+++ Group2Week11/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Group2Week11.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
